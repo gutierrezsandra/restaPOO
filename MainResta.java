bff278d2c625ea266270a17beff6cc75a6c08c68
sandra gutierrez <sandraivonneans18@gmail.com>
2024-04-22 19:50:06 -0600

    restaPOO

---
+++ MainResta.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class MainResta {
+    public static void main(String[] args) {
+        Resta n1 = new Resta();
+        Scanner num= new Scanner (System.in);
+        System.out.println("Resta de 2 numeros");
+        System.out.println("Ingrese un numero:");
+        int a= num.nextInt();
+        n1.a=a;
+        System.out.println("Ingrese segundo1 numero:");
+        int b= num.nextInt();
+        n1.b=b;
+        System.out.println("El resultado es:");
+        System.out.println(n1.restar());
+    
+    }
+}
---
+++ Resta.java
@@ -0,0 +1,7 @@
+public class Resta{
+    int a, b, r;
+    public int restar(){
+        r= this.a-this.b;
+        return r;
+    }
+}
