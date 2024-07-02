a2d28fe32bb2eff9643dab1c189c09a5f2f61582
gutierrezsandra <167218740+gutierrezsandra@users.noreply.github.com>
2024-07-02 14:43:34 -0600

    Update MainResta.java

--- MainResta.java
+++ MainResta.java
@@ -1,17 +1,26 @@
 import java.util.Scanner;
+
+class Resta {
+    int a;
+    int b;
+
+    int restar() {
+        return a - b;
+    }
+}
+
 public class MainResta {
     public static void main(String[] args) {
         Resta n1 = new Resta();
-        Scanner num= new Scanner (System.in);
+        Scanner num = new Scanner(System.in);
         System.out.println("Resta de 2 numeros");
         System.out.println("Ingrese un numero:");
-        int a= num.nextInt();
-        n1.a=a;
-        System.out.println("Ingrese segundo1 numero:");
-        int b= num.nextInt();
-        n1.b=b;
+        int a = num.nextInt();
+        n1.a = a;
+        System.out.println("Ingrese segundo numero:");
+        int b = num.nextInt();
+        n1.b = b;
         System.out.println("El resultado es:");
         System.out.println(n1.restar());
-    
     }
 }
